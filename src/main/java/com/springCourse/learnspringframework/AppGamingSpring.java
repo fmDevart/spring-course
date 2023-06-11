b6429e45ca1b035bb80d142ebbb142f915947608
ag4tor <mesuracafrancesco@outlook.it>
2023-06-11 02:03:21 +0200

    changed config files

--- src/main/java/com/springCourse/learnspringframework/AppGamingSpring.java
+++ src/main/java/com/springCourse/learnspringframework/AppGamingSpring.java
@@ -1,12 +1,32 @@
 package com.springCourse.learnspringframework;
 
 import game.GameRunner;
+import game.GamingConsole;
+import game.MarioGame;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
 public class AppGamingSpring
 {
+
+    @Bean
+    public GamingConsole game(){
+
+        MarioGame game = new MarioGame();
+        return game;
+    }
+
+    @Bean
+    public GameRunner gameRunner( GamingConsole game){
+
+        var gameRunner = new GameRunner(game);
+        return  gameRunner;
+    }
     public static void main(String[] args) {
-        var context = new AnnotationConfigApplicationContext(AppGamingConfig.class);
+        var context = new AnnotationConfigApplicationContext(AppGamingSpring.class);
 
         context.getBean(GameRunner.class).run();
 
